22a77fa173fa0fecbc720e64bb8f3530e4ad76c0
isadorabom <isadora.santos@novaandradina.org>
2019-12-12 12:48:07 -0300

    declaração não utilizada removida

--- frontend/src/pages/Reports/index.js
+++ frontend/src/pages/Reports/index.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import "./reports.css";
 import AppArea from "../../components/AppArea";
-import api from "../../services/api";
 
 export default function Reports() {
 	return <AppArea id="reports"></AppArea>;
